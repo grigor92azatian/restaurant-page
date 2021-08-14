390e77a1dab468613526b83d0ecb2047a3f36f18
Grigor Azatian <grigorazatian@yahoo.com>
2021-08-13 19:18:59 -0700

    created a fixed-to-top navbar and added blank js files that will populate the pages

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import "./styles.css";
 import initialPageLoad from "./initial-page-load.js";
 
 initialPageLoad();
+
+//------------------------------------------------------------------Navbar Tabs-------------------------------------------
+
+//Home
+
+//Menu
+
+//Contact (include yelp and google maps link here)
+
+//Order Now
+
+
