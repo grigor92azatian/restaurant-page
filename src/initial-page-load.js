a6754949cd9d1b1bfe4a21440ab0de17c3029c8c
Grigor Azatian <grigorazatian@yahoo.com>
2021-08-17 18:31:17 -0700

    added class names to a few divs that did not have one to organize the css a bit better

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -7,8 +7,10 @@ export default function initialPageLoad(){
     bannerPic.setAttribute("src", "https://cdn2.lamag.com/wp-content/uploads/sites/6/2018/10/best-ayce-sushi-los-angeles-county-Nattapol-Poonpiriya-EyeEm-getty-images-1-1068x712.jpg");
     bannerPic.setAttribute("alt", "sushi-roll-photo");
 
+    restaurantName.className = "restaurantName";
     restaurantName.innerHTML = "<span class='hikari'>Hikari</span> Sushi";
 
+    restaurantInfo.className = "initialPageText";
     restaurantInfo.innerHTML = "Come on in and enjoy some of the best sushi in the valley! We have a large selection of hand rolls, nigiri, and specialty rolls to choose from! " +
                                 "A lot of our guests love our all you can eat option but we also offer a la carte and takeout. If what you're looking for is fresh and tasty " +
                                 "sushi, look no further!";
