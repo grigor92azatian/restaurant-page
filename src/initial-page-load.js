ca282a8bc734a0d63d4a82bb7e3ca8ee9b6d94bb
Grigor Azatian <grigorazatian@yahoo.com>
2021-08-18 17:42:34 -0700

    gave a white tint to the initial page banner picture and changed the photo width to 100%

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -1,12 +1,15 @@
 export default function initialPageLoad(){
     let pageContent = document.querySelector("#content");
+    let bannerContainer = document.createElement("div");
     let bannerPic = document.createElement("img");
     let restaurantName = document.createElement("h1");
     let restaurantInfo = document.createElement("p");
 
+    bannerContainer.className = "bannerPicContainer";
+
     bannerPic.setAttribute("src", "https://cdn2.lamag.com/wp-content/uploads/sites/6/2018/10/best-ayce-sushi-los-angeles-county-Nattapol-Poonpiriya-EyeEm-getty-images-1-1068x712.jpg");
     bannerPic.setAttribute("alt", "sushi-roll-photo");
-
+    
     restaurantName.className = "restaurantName";
     restaurantName.innerHTML = "<span class='hikari'>Hikari</span> Sushi";
 
@@ -14,7 +17,8 @@ export default function initialPageLoad(){
     restaurantInfo.innerHTML = "Come on in and enjoy some of the best sushi in the valley! We have a large selection of hand rolls, nigiri, and specialty rolls to choose from! " +
                                 "A lot of our guests love our all you can eat option but we also offer a la carte and takeout. If what you're looking for is fresh and tasty " +
                                 "sushi, look no further!";
-    pageContent.appendChild(bannerPic);
+    bannerContainer.appendChild(bannerPic);
+    pageContent.appendChild(bannerContainer);
     pageContent.appendChild(restaurantName);
     pageContent.appendChild(restaurantInfo);
 }
---
+++ src/menu-tab.js
@@ -0,0 +1,24 @@
+/*
+A la carte Menu
+    Appetizer
+    Salad
+    Noodles
+    Bento Box Special 
+    Sashimi Combo
+    Sushi Combo
+    Sushi and Sashimi
+    Beverage
+    Kid's Meal
+    Soft Drinks
+    Dessert
+
+Sushi rolls
+    Fresh Rolls
+    Bakes Rolls
+    Tempura Rolls
+    Regular Rolls
+
+AYCE menu
+
+Happy Hour menu
+*/
