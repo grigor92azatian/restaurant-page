e81e350753d4809b72b5ee57bf6ea9f6a2985b16
Grigor Azatian <34463837+grigor92azatian@users.noreply.github.com>
2021-08-26 15:58:51 -0700

    work on info page
    added a banner photo, a map icon, some contact info

---
+++ src/footer-bar.js
@@ -0,0 +1,5 @@
+//footer that appears on all pages that has links to yelp, instagram, and google maps
+//yelp link - https://www.yelp.com/biz/hikari-sushi-chatsworth-2?utm_campaign=www_business_share_popup&utm_medium=copy_link&utm_source=(direct)
+//instagram link - https://www.instagram.com/hikarisushichatsworth/?hl=en
+//google maps link - https://g.page/hikarisushichatsworth?share
+
---
+++ src/info-tab.js
@@ -0,0 +1,15 @@
+function contactPage(){
+    
+}
+
+/*
+Location - google maps link
+phone number
+business hours
+yelp link
+*/
+
+//banner photo of outside of restaurant
+//address, hours, phone number
+//google maps link
+//yelp link
