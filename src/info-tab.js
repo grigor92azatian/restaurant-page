a7add19caae6d6ac8104f6628805a41ac63c875e
Grigor Azatian <34463837+grigor92azatian@users.noreply.github.com>
2021-08-27 18:00:01 -0700

    basic layout of the info page has been set via hardcoded html in the index file which now needs to be transferred into dynamic js in the info tab.js file

--- src/info-tab.js
+++ src/info-tab.js
@@ -2,14 +2,3 @@ function contactPage(){
     
 }
 
-/*
-Location - google maps link
-phone number
-business hours
-yelp link
-*/
-
-//banner photo of outside of restaurant
-//address, hours, phone number
-//google maps link
-//yelp link
